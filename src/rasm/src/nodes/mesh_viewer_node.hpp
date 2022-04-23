9ecff33934d84d2a7b4f4a6b7f8cca0614622a7d
AshwinNehete <ashwinnehete@gmail.com>
2022-04-23 14:51:12 -0400

    Add moonranger typedefs

--- src/rasm/src/nodes/mesh_viewer_node.hpp
+++ src/rasm/src/nodes/mesh_viewer_node.hpp
@@ -21,8 +21,59 @@
 
 #include "read_triangle_mesh.h"
 
-#define MESH_MAX_GLOBAL_FACES 1000
-#define MESH_CHANNELS 3
+typedef uint32_t uint32;
+typedef double float64;
+
+/******************************************************************************/
+/*                  Type definition (MOONRANGER pose)
+/******************************************************************************/
+
+typedef struct {
+  CFE_TIME_SysTime_t  timeStamp; 
+  float64             x_pos;     // meters
+  float64             y_pos;     // meters
+  float64             z_pos;     // meters
+  float64             x_quat;    // quaternion
+  float64             y_quat;    // quaternion
+  float64             z_quat;    // quaternion
+  float64             w_quat;    // quaternion
+  float64             covariance[36];
+} MOONRANGER_Pose_t;
+
+/******************************************************************************/
+/*                           Terrain Mesh
+/******************************************************************************/
+
+#define MESH_MAX_GLOBAL_FACES 100000 // max number of faces allowed in mesh
+#define MESH_CHANNELS 3 // number of channels per mesh container
+
+/**
+ * @brief Mesh type to be used internally in an app
+ */
+typedef struct {
+
+    // vertices
+    float Vertices[MESH_MAX_GLOBAL_FACES][MESH_CHANNELS];
+    uint32 numVertices;
+    
+    // faces
+    int Faces[MESH_MAX_GLOBAL_FACES][MESH_CHANNELS];
+    uint32 numFaces;
+    bool isTriangulated = false;
+
+    // timestamp
+    CFE_TIME_SysTime_t Timestamp;
+
+    // associated pose (0s for world meshes)
+    MOONRANGER_Pose_t MeshPose;
+    
+    // mesh counter
+    uint32 MeshId = 0;
+    
+    // flag if world mesh (defaults to not a world mesh)
+    bool AccumFlag = false;
+
+} MOONRANGER_Mesh_t;
 
 /************************************
  * Mesh class
